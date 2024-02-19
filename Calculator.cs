e572320299fcdfb9c8b4822e19fcb05db18376d7
Bharathi <bharathitestsys@gmail.com>
2024-02-19 11:18:33 +0530

    First COmmit

---
+++ Calculator.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Numerics;
+
+namespace CalculatorApp
+{
+    public class Functions
+    {
+        public static T Addition<T>(T input1, T input2)
+        {
+            bool isValid = ValidateInput(input1, input2);
+            if (isValid)
+            {
+                dynamic value1 = input1;
+                dynamic value2 = input2;
+                return  value1 + value2;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input - Both inputs must be numeric types");
+
+            }
+            
+        }
+
+        public static string AcheckString(string value1, string value2)
+        {
+            throw new ArgumentException("Invalid input - Both inputs must be numeric types");
+        }
+
+        public static T Subtract<T>(T input1, T input2)
+        {
+            bool isValid = ValidateInput(input1, input2);
+            if (isValid)
+            {
+                dynamic value1 = input1;
+                dynamic value2= input2;
+                return value1 - value2;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input - Both inputs must be numeric types");
+
+            }
+
+
+        }
+
+        public static T Multiply<T>(T input1, T input2)
+        {
+            bool isValid = ValidateInput(input1, input2);
+            if (isValid)
+            {
+                dynamic value1 = input1;
+                dynamic value2 = input2;
+                return value1 * value2;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid input - Both inputs must be numeric types");
+
+            }
+        }
+
+        public static T Divide<T>(T input1, T input2)
+        {
+            bool isValid = ValidateInput(input1, input2);
+            if (isValid)
+            {
+                dynamic value1 = input1;
+                dynamic value2  = input2;
+                
+                return value1 / value2; 
+            }
+           else
+            {
+                throw new ArgumentException("Invalid input - Both inputs must be numeric types");
+            }
+        }
+
+        private static bool ValidateInput<T>(T input1, T input2)
+        {
+            return IsNumeric(input1) && IsNumeric(input2);
+        }
+
+        private static bool IsNumeric<T>(T value)
+        {
+            return value is sbyte || value is byte ||
+                   value is short || value is ushort ||
+                   value is int || value is uint ||
+                   value is long || value is ulong ||
+                   value is float || value is double || value is decimal ||
+                   value is BigInteger;
+        }
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,111 @@
+﻿// CalculatorFunctionsTest.cs
+
+using NUnit.Framework;
+using CalculatorApp;
+using System;
+using NUnit.Framework.Legacy;
+using System.Numerics;
+using Newtonsoft.Json.Linq;
+
+namespace CalculatorApp.Tests
+{
+    [TestFixture]
+    public class CalculatorFunctionsTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase("cs", "hih")]
+        [TestCase(1 / 2, 3 / 4)]    
+        [TestCase(2.22, "ss")]
+        [TestCase(22345768907899, 34363848940)]
+        [TestCase(-29, 89)]
+        public void Adding_twonumbers(object input1, object input2)
+        {
+            //Arrange
+            dynamic value1 = input1;
+            dynamic value2= input2;
+
+            //Act
+            object result = Functions.Addition(input1, input2);
+
+            //assert
+          ClassicAssert.AreEqual(value1+value2, result,"Invalid Inputs");
+
+
+
+        }
+
+
+
+        [Test]
+        [TestCase("cs", "hih")]
+        [TestCase(1 / 2, 3 / 4)]
+        [TestCase(2.22, 3.33)]
+        [TestCase(-22345768907899, 34363848940)]
+        [TestCase(-29, 89)]
+        public void Subtracting_Twonumbers(object input1, object input2)
+        {
+
+            //Arrange
+            dynamic value1 = input1;
+            dynamic value2 = input2;
+
+            //Act
+            object result = Functions.Subtract(input1, input2);
+
+            //Assert 
+            ClassicAssert.AreEqual(value1-value2, result, "Invalid Inputs");
+
+
+        }
+
+        [Test]
+            [TestCase('s', 's')]
+            [TestCase(1 / 2, 3 / 4)]
+            [TestCase(2.22, 3.33)]
+            [TestCase(223, 676)]
+            [TestCase(-29, 89)]
+            public  void Multiplying_TwoNumbers(object input1, object input2)
+            {
+            //Arrange
+
+            dynamic value1 = input1;
+            dynamic value2= input2;
+
+            //Act
+            object result = Functions.Multiply(input1, input2);
+
+            //Assert 
+            ClassicAssert.AreEqual(value1 * value2, result, "Invalid Inputs");
+
+
+
+        }
+
+
+            [Test]
+            [TestCase("cs", "hih")]
+            [TestCase(33, 33)]
+            [TestCase(22, 33)]
+            [TestCase(288, 66)]
+            [TestCase(773, 89)]
+            public void Division_ofTwoNumbers(object input1, object input2)
+            {
+
+            //Arrange
+            dynamic value1 = input1;
+            dynamic value2 = input2;
+
+            //Act
+            object result = Functions.Divide(input1, input2);
+
+            //assert
+            ClassicAssert.AreEqual(value1 - value2, result, "Invalid Inputs");
+        }
+    }
+}
+
